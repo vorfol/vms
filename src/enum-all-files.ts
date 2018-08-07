3335237c8eeaf1d1ff6612030e6e200287ddcdab
Sergey Vorfolomeev <svorfolomeev@appliedtech.ru>
2018-08-07 17:41:54 +0500

    a

--- src/enum-all-files.ts
+++ src/enum-all-files.ts
@@ -6,6 +6,7 @@ import ToOutputChannel from './output-channel';
 export default async function EnumAllFiles() {
     if (workspace.workspaceFolders) {
         workspace.workspaceFolders.forEach((folder) => {
+            //BUG: for MemFS does start search from "file://d:", not from "memfs:/"
             workspace.findFiles(new RelativePattern(folder, '*'))
             .then((uris) => {
                 uris.forEach((uri) => {
