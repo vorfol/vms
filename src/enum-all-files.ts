2407a2218c66f6ec2a9288e86b9851f3eeeef217
Sergey Vorfolomeev <svorfolomeev@appliedtech.ru>
2018-08-10 15:56:01 +0500

    now i can create and run vsix :)

--- src/enum-all-files.ts
+++ src/enum-all-files.ts
@@ -1,20 +1,27 @@
 import {workspace} from 'vscode';
-import {RelativePattern} from 'vscode';
 
 import {ToOutputChannel} from './output-channel';
 
 /** Enumerate all files in all workspaces. For test only.
 */
 export async function EnumAllFiles() {
-    if (workspace.workspaceFolders) {
-        workspace.workspaceFolders.forEach((folder) => {
-            //BUG: for MemFS does start search from "file://d:", not from "memfs:/"
-            workspace.findFiles(new RelativePattern(folder, '*'))
-            .then((uris) => {
-                uris.forEach((uri) => {
-                    ToOutputChannel(uri.toString());
-                });
-            });
+    // //workspace.registerFileSearchProvider
+    // if (workspace.workspaceFolders) {
+    //     workspace.workspaceFolders.forEach((folder) => {
+    //         //BUG: findFiles ignores sheme
+    //         workspace.findFiles(new RelativePattern(folder, '*'))
+    //         .then((uris) => {
+    //             uris.forEach((uri) => {
+    //                 ToOutputChannel(folder.name + ' : ' + folder.uri.scheme + ' : ' + uri.toString());
+    //             });
+    //         });
+    //     });
+    // }
+
+    //Now this version walk around all schemes, but need FileSearchProvider to be implemented on that scheme
+    workspace.findFiles('**/*').then((allFiles) => {
+        allFiles.forEach((file) => {
+            ToOutputChannel(file.toString());
         });
-    }
+    });
 }
