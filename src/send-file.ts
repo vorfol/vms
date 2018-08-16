8f3257d05e3480fb40b3fd3c0d9a1a21b6cd36ba
Sergey Vorfolomeev <svorfolomeev@appliedtech.ru>
2018-08-16 14:51:38 +0500

    some...

--- src/send-file.ts
+++ src/send-file.ts
@@ -16,11 +16,14 @@ const mTimeTreshold = 2;    //two seconds
  */
 export function SendFile(sftp : SFTPWrapper, file : Uri ) : Promise<boolean> {
     let relativeFile = workspace.asRelativePath(file);
+    //TODO: verbose? silent?
     ToOutputChannel(`Sending file: ${relativeFile}`);
     return new Promise(async (resolve : (ok:boolean) => void, reject : (error:Error) => void) => {
         //TODO: do read from VFS and open-write-close sftp. Now only local FS is supported
         try {
+            //get local file attributes
             let localStat = await localStatFn(file.fsPath);
+            //get remote file attributes
             let sftpStat = await new Promise<Stats | undefined>((resolveStat, rejectStat) => {
                 sftp.stat(relativeFile, (err, stats) => {
                     if (err) {
@@ -31,20 +34,23 @@ export function SendFile(sftp : SFTPWrapper, file : Uri ) : Promise<boolean> {
                     }
                 })
             });
+            //do not send if size and modification time are the same
             if (sftpStat &&
                 localStat.size === sftpStat.size &&
-                Math.abs(localStat.mtimeMs/1000 - sftpStat.mtime) < mTimeTreshold )  //besause mtimeMs is too big to be integer, and sftpStat.mtime in seconds!
+                Math.abs(localStat.mtimeMs/1000 - sftpStat.mtime) < mTimeTreshold )  //sftpStat.mtime in seconds!
             {
+                //TODO: verbose? silent?
                 ToOutputChannel(`File: ${relativeFile} has not been altered`);
                 resolve(false); //file not sent, but not reject this operation
             }   
             else {
+                //send
                 sftp.fastPut(file.fsPath, relativeFile, (error: Error) => {
                     if (error) {
                         reject(error);  //error while sending file, reject operation
                     }
                     else {
-                        //Call sftp.SETSTAT, setting size and mTimeMs will be enough
+                        //set size and time
                         let attrs :InputAttributes  = {
                             size: localStat.size,
                             mtime: localStat.mtime, //as Date
@@ -54,6 +60,7 @@ export function SendFile(sftp : SFTPWrapper, file : Uri ) : Promise<boolean> {
                             if (err) {
                                 reject(err);
                             } else {
+                                //TODO: verbose? silent?
                                 ToOutputChannel(`File: ${relativeFile} has been sent`);
                                 resolve(true);  //file sent successfully        
                             }
