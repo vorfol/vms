f40105acf3429c95bbf4f903fb446e789aa3d1c7
Sergey Vorfolomeev <svorfolomeev@appliedtech.ru>
2018-08-16 10:48:08 +0500

    check file STAT before sending

--- src/send-file.ts
+++ src/send-file.ts
@@ -1,9 +1,13 @@
-
+import * as fs from 'fs';
+import * as util from 'util';
 import {workspace, Uri} from 'vscode';
 import {SFTPWrapper} from 'ssh2';
+import {Stats} from 'ssh2-streams';
 
 import {ToOutputChannel} from './output-channel';
 
+const localStatFn = util.promisify(fs.stat);
+const mTimeTreshold = 100;
 /**
  * Send file using SFTP client
  * 
@@ -13,16 +17,40 @@ import {ToOutputChannel} from './output-channel';
 export function SendFile(sftp : SFTPWrapper, file : Uri ) : Promise<boolean> {
     let relativeFile = workspace.asRelativePath(file);
     ToOutputChannel(`Sending file: ${relativeFile}`);
-    return new Promise((resolve : (ok:boolean) => void, reject : (error:Error) => void) => {
-        //TODO: do read from VFS and open-write-close sftp
-        sftp.fastPut(file.fsPath, relativeFile, (error: Error) => {
-            if (error) {
-                reject(error);
-            }
+    return new Promise(async (resolve : (ok:boolean) => void, reject : (error:Error) => void) => {
+        //TODO: do read from VFS and open-write-close sftp. Now only local FS is supported
+        try {
+            let localStat = await localStatFn(file.fsPath);
+            let sftpStat = await new Promise<Stats>((resolveStat, rejectStat) => {
+                sftp.stat(relativeFile, (err, stats) => {
+                    if (err) {
+                        rejectStat(err);    //INNER PROMISE: will be cought in catch below
+                    }
+                    else {
+                        resolveStat(stats); //INNER PROMISE: returned to sftpStaf
+                    }
+                })
+            });
+            if (localStat.size === sftpStat.size &&
+                Math.abs(localStat.mtimeMs - sftpStat.mtime) < mTimeTreshold )  //besause mtimeMs is too big to be integer
+            {
+                resolve(false); //file not sent, but not reject this operation
+            }   
             else {
-                resolve(true);
+                sftp.fastPut(file.fsPath, relativeFile, (error: Error) => {
+                    if (error) {
+                        reject(error);  //error while sending file, reject operation
+                    }
+                    else {
+                        //TODO: call sftp.SETSTAT, align mTime
+                        resolve(true);  //file sent successfully
+                    }
+                });
             }
-        });
+        }
+        catch(err) {
+            reject(err);
+        }
     });
 }
 
